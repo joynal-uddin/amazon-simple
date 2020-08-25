880c7ca8339c7cab69d052d61b438ae15fac3779
joynal-uddin <joynaluddinbhuiyan2@.com>
2020-08-25 13:04:52 +0600

    it is our another commit

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import fakeData from '../../fakeData';
+import './Shop.css';
+import Product from '../Product/Product';
+import Cart from '../Cart/Cart';
+
+const Shop = () => {
+    const first10 = fakeData.slice(0,10);
+    const [products, setProducts] = useState(first10);
+    const [cart, setCart] = useState([]);
+    const handleAddProduct =(product) =>{
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
+    return (
+        <div className="shopContainer">
+            <div className="productContainer">
+                {
+                    products.map(product => <Product
+                        handleAddProduct = {handleAddProduct}
+                         product={product}>
+                         </Product>)
+                }
+            </div>
+           <div className="cardContainer">
+               <Cart cart = {cart}></Cart>
+           </div>          
+        </div>
+    );
+};
+
+export default Shop;
